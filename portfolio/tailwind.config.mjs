d03083ac8288236f99dffaad3fbfd3a5c7965079
an-siuu-man <ansuman_sharma@protonmail.com>
2024-12-26 15:51:40 -0600

    initial commit + basic navbar

---
+++ portfolio/tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+
+      fontFamily: {
+        poppins: ["Poppins"],
+        inter: ["Inter Tight"],
+        dmsans: ["DM Sans"],
+        cursive: ["Parisienne"],
+      },
+    },
+  },
+  plugins: [],
+};
