c492473048940b06d369b9cbdb5e2cbc131493aa
an-siuu-man <ansuman_sharma@protonmail.com>
2025-04-01 00:53:52 -0500

    made new project card

--- portfolio/src/app/components/Projects.js
+++ portfolio/src/app/components/Projects.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 import Project from './Project';
-
+import TestProject from './TestProject';
 
 import project1 from '../../../public/project1.png';
 import project2 from '../../../public/hackmidterms.jpg';
 import project3 from '../../../public/project3.png';
 import project4 from '../../../public/project4.png';
-
+import project5 from '../../../public/project5.jpg';
 
 
 
@@ -58,13 +58,17 @@ export default function Projects() {
                         </p>
                     </div>
                 </div>
-                <div className='projects-container grid grid-cols-1 md:grid-cols-2 gap-4 items-start justify-center max-w-[80vw]'>
-                    <Project image={project1} github="https://github.com/an-siuu-man/code-lingo" devpost='https://devpost.com/software/codelingo-lg9a4q' skills={['Next.js', 'PostgreSQL', 'Flask']} title='CodeLingo</>' description='Winner in Hack K-State 2024, this web app helps users learn C++ with ease by letting them visualize their code in the form of dynamic boxes showing how variables are updated and functions are called.' />
-                    <Project image={project2} github="https://github.com/an-siuu-man/hackmidterms" devpost="https://devpost.com/software/hackmidterms" skills={['Next.js', 'Pinata IPFS', 'Whisper by OpenAI', 'Flask']} title='HackMidterms' description='This web app helps users, especially students, create flashcards and practice questions from their online lectures through a speech-to-text model for quick revision and make studying easier.' />
-                    <Project image={project3} github="https://github.com/an-siuu-man/geogo" devpost="https://devpost.com/software/geogo" skills={['Django', 'Flask', 'SerpAPI', 'Gemini']} title='Geogo' description='To make tourism easier, me and three of my peers developed this app to provide users with visa requirements, flight itineraries, popular tourist spots, and potential daily costs of traveling to any city with an international airport.' />
-                    <Project image={project4} github="https://github.com/manees-singh/NeoPyramid" devpost='https://devpost.com/software/servo-tower' skills={['C/C++', "Arduino", 'Raspberry Pi']} title='NeoPyramid' description='This project is a mechanical rubber-band shooting turret with a rotating barrel that can detect foreign objects and shoot accurately at them at distances more than 25ft.' />
+
+                <div className='projects-container flex flex-row justify-start gap-4 items-start flex-wrap w-full max-w-[90vw]'>
+                    <TestProject image={project1} github="https://github.com/an-siuu-man/code-lingo" devpost='https://devpost.com/software/codelingo-lg9a4q' skills={['Next.js', 'PostgreSQL', 'Flask']} title='CodeLingo</>' description='CodeLingo was created to help beginners understand code flow and structure, addressing the challenges of traditional learning methods. By turning code execution into interactive visuals, it makes complex concepts more accessible and easier to grasp. The goal is to help learners build confidence and deepen their understanding of programming through clear, engaging visuals.' />
+                    <TestProject image={project2} github="https://github.com/an-siuu-man/hackmidterms" devpost="https://devpost.com/software/hackmidterms" skills={['Next.js', 'Pinata IPFS', 'Whisper by OpenAI', 'Flask']} title='HackMidterms' description='This web app helps users, especially students, create flashcards and practice questions from their online lectures through a speech-to-text model for quick revision and make studying easier.' />
+                    <TestProject image={project3} github="https://github.com/an-siuu-man/geogo" devpost="https://devpost.com/software/geogo" skills={['Django', 'Flask', 'SerpAPI', 'Gemini']} title='Geogo' description='To make tourism easier, me and three of my peers developed this app to provide users with visa requirements, flight itineraries, popular tourist spots, and potential daily costs of traveling to any city with an international airport.' />
+                    <TestProject image={project4} github="https://github.com/manees-singh/NeoPyramid" devpost='https://devpost.com/software/servo-tower' skills={['C/C++', "Arduino", 'Raspberry Pi']} title='NeoPyramid' description='This project is a mechanical rubber-band shooting turret with a rotating barrel that can detect foreign objects and shoot accurately at them at distances more than 25ft.' />
+                    <TestProject image={project5} title='SafeChain' skills={['React.js', 'Express.js', 'Pinata IPFS', 'PostgreSQL']} description='SafeChain is a decentralized communication system that ensures connectivity during natural disasters when traditional networks fail. It uses peer-to-peer communication, decentralized storage, and blockchain for secure, efficient information sharing. The goal is to improve disaster response by providing a reliable network that works without traditional infrastructure, saving lives and boosting coordination.' />
+
+                    {/* <p className='font-dmsans text-xl my-10'>More coming soon!</p> */}
                 </div>
-                <p className='font-dmsans text-xl my-10'>More coming soon!</p>
+                
             </div>
         </div>
     );
